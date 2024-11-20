2aa6570346e6d701355d51e139089de460aaf13b
Bojan Endrovski <bojan.endrovski@gmail.com>
2024-11-20 16:01:44 +0100

    🐦 wren sample running

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,73 @@
 #include <iostream>
+#include "wren.hpp"
 
-int main(void) {
-    std::cout << "Hello, World!" << std::endl;
+extern "C" {
+    #include "wren_debugger.h"
+    #include "wren_vm.h"
+}
+
+
+static void writeFn(WrenVM* vm, const char* text)
+{
+  printf("%s", text);
+}
+
+const char* get_module_path(WrenVM* vm, const char* module, const char* root) 
+{
+    return NULL;    
+}
+
+void errorFn(WrenVM* vm, WrenErrorType errorType,
+             const char* module, const int line,
+             const char* msg)
+{
+  switch (errorType)
+  {
+    case WREN_ERROR_COMPILE:
+    {
+      printf("[%s line %d] [Error] %s\n", module, line, msg);
+    } break;
+    case WREN_ERROR_STACK_TRACE:
+    {
+      printf("[%s line %d] in %s\n", module, line, msg);
+    } break;
+    case WREN_ERROR_RUNTIME:
+    {
+      printf("[Runtime Error] %s\n", msg);
+    } break;
+  }
+}
+
+int main()
+{
+  WrenConfiguration config;
+  wrenInitConfiguration(&config);
+    config.writeFn = &writeFn;
+    config.errorFn = &errorFn;    
+    #if WREN_DEBUGGER
+      config.modulePathFn = &get_module_path;
+      config.debuggerPort = "8080";
+      config.enableDebugger = true;
+    #endif
+  WrenVM* vm = wrenNewVM(&config);
+
+  const char* module = "main";
+  const char* script = "System.print(\"I am running in a VM!\")";
+
+  //debuggerAddBreakpoint(&vm->debugger, module, 1);
+
+  wrenDebuggerPollConfigCmds(vm);
+  WrenInterpretResult result = wrenInterpret(vm, module, script);
+
+  switch (result) {
+    case WREN_RESULT_COMPILE_ERROR:
+      { printf("Compile Error!\n"); } break;
+    case WREN_RESULT_RUNTIME_ERROR:
+      { printf("Runtime Error!\n"); } break;
+    case WREN_RESULT_SUCCESS:
+      { printf("Success!\n"); } break;
+  }
+
+  wrenFreeVM(vm);
 }
 
